52cc832f0570054f2e0f78cb2637b8e1d08ea379
wasalas <wasalas893@gmail.com>
2020-07-06 09:37:27 +0530

    create the profileActivity and logout firebase

--- InstaApp/app/src/main/java/com/example/instaapp/ProfileActivity.kt
+++ InstaApp/app/src/main/java/com/example/instaapp/ProfileActivity.kt
@@ -5,29 +5,25 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import com.google.firebase.auth.FirebaseAuth
 
-class PostsActivity : AppCompatActivity() {
+class ProfileActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_posts)
+        setContentView(R.layout.activity_profile)
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.menu_posts,menu)
+        menuInflater.inflate(R.menu.menu_profile,menu)
         return super.onCreateOptionsMenu(menu)
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        if(item.itemId==R.id.menu_profile){
-            val intent=Intent(this,ProfileActivity::class.java)
-            startActivity(intent)
+        if(item.itemId==R.id.menu_logout){
+            FirebaseAuth.getInstance().signOut()
+            startActivity(Intent(this,LoginActivity::class.java))
         }
-
         return super.onOptionsItemSelected(item)
     }
-
-
-
-
 }
