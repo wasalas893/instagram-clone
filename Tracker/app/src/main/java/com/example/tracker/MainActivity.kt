eac19cc06adae3fb79c2731750e6402a1081cb6b
wasalas <wasalas893@gmail.com>
2020-07-09 10:15:39 +0530

    get the api covide data natinala data and state adata

--- Tracker/app/src/main/java/com/example/tracker/MainActivity.kt
+++ Tracker/app/src/main/java/com/example/tracker/MainActivity.kt
@@ -2,11 +2,67 @@ package com.example.tracker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import android.util.Log
+import com.google.gson.GsonBuilder
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+private  const val BASE_URL="https://covidtracking.com/api/v1/"
+private  const val TAG="MainActivity"
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var perStateDailyData: Map<String, List<CovidData>>
+    private lateinit var nationalDailyData: List<CovidData>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val gson=GsonBuilder().setDateFormat("yyyy-MM-dd'T'HH:mm:ss").create()
+        val retrofit=Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create(gson))
+            .build()
+
+        val covidService=retrofit.create(CovidService::class.java)
+        //Fetch the national data
+        covidService.getNationalData().enqueue(object :Callback<List<CovidData>>{
+            override fun onFailure(call: Call<List<CovidData>>, t: Throwable) {
+               Log.e(TAG,"onFailure $t")
+            }
+
+            override fun onResponse(call: Call<List<CovidData>>, response: Response<List<CovidData>>) {
+               Log.i(TAG,"onResponse $response")
+                val nationalData=response.body()
+                if(nationalData==null){
+                    Log.w(TAG,"Did not receive a valid response body")
+                    return
+                }
+                nationalDailyData=nationalData.reversed()
+                Log.i(TAG,"update graph with national data")
+            }
+
+        })
+
+        //Fetch the state data
+        covidService.getStatesData().enqueue(object :Callback<List<CovidData>>{
+            override fun onFailure(call: Call<List<CovidData>>, t: Throwable) {
+                Log.e(TAG,"onFailure $t")
+            }
+
+            override fun onResponse(call: Call<List<CovidData>>, response: Response<List<CovidData>>) {
+                Log.i(TAG,"onResponse $response")
+                val statesData=response.body()
+                if(statesData==null){
+                    Log.w(TAG,"Did not receive a valid response body")
+                    return
+                }
+                perStateDailyData=statesData.reversed().groupBy { it.state }
+                Log.i(TAG,"update spinner with state names")
+            }
+
+        })
     }
 }
